0f84761032ffa454617b36c8da5eeb02ac6430d9
pingdrew <pingdrew0@gmail.com>
2023-08-19 15:54:07 -0500

    seed folder finished

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentsData = require('./commentsData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+
+  const comments = await Comment.bulkCreate(commentsData, {
+    returning: true,
+  });
+
+
+
+  process.exit(0);
+};
+
+seedDatabase();
